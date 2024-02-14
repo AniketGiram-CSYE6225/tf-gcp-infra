55a035e8da83443ab83a512f5e72a30f18b095e0
Aniketgiram <giram.a@northeastern.edu>
2024-02-14 18:25:51 -0500

    terraform template added and verified with github workflow

---
+++ infra.tf
@@ -0,0 +1,39 @@
+provider "google" {
+  project = "nscc-414218"
+  region  = "us-east1"
+}
+
+resource "google_compute_network" "nscc_vpc" {
+  name                            = "nscc"
+  auto_create_subnetworks         = false
+  routing_mode                    = "REGIONAL"
+  delete_default_routes_on_create = true
+}
+
+resource "google_compute_subnetwork" "webapp" {
+  name                     = "webapp"
+  ip_cidr_range            = "255.0.0.0/24"
+  region                   = "us-east1"
+  network                  = google_compute_network.nscc_vpc.id
+  depends_on               = [google_compute_network.nscc_vpc]
+  private_ip_google_access = true
+  purpose                  = "PUBLIC"
+}
+
+resource "google_compute_subnetwork" "db" {
+  name                     = "db"
+  ip_cidr_range            = "255.0.1.0/24"
+  region                   = "us-east1"
+  network                  = google_compute_network.nscc_vpc.id
+  depends_on               = [google_compute_network.nscc_vpc]
+  private_ip_google_access = true
+  purpose                  = "PRIVATE"
+}
+
+resource "google_compute_route" "webapp_route" {
+  name             = "webapp-route"
+  dest_range       = "0.0.0.0/0"
+  network          = google_compute_network.nscc_vpc.id
+  next_hop_gateway = "default-internet-gateway"
+  tags             = ["webapp"]
+}
