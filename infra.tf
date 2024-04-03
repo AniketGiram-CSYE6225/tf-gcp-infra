1c0599829c5cee0110f54de017df7b811f69d3f1
Aniketgiram <giram.a@northeastern.edu>
2024-04-02 23:20:56 -0400

    ingress firewall added for health check

--- infra.tf
+++ infra.tf
@@ -71,7 +71,7 @@ resource "google_dns_record_set" "a_record" {
   managed_zone = var.managed_zone
   type         = var.record_type
   ttl          = var.dns_ttl
-  rrdatas = [google_compute_address.default.address]
+  rrdatas      = [google_compute_address.default.address]
 }
 
 resource "google_service_account" "nscc_service_account" {
