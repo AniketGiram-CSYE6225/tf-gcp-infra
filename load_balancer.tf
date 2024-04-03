d269f6d97b9d7aa58c08a4913d3d31dc8c93944a
Aniketgiram <giram.a@northeastern.edu>
2024-04-02 23:23:01 -0400

    loadbalancer backend added

--- load_balancer.tf
+++ load_balancer.tf
@@ -4,3 +4,18 @@ resource "google_compute_address" "default" {
   network_tier = "STANDARD"
   region       = var.region
 }
+
+resource "google_compute_region_backend_service" "default" {
+  name                  = "l7-xlb-backend-service"
+  region                = var.region
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  health_checks         = [google_compute_region_health_check.autohealing.id]
+  protocol              = "HTTP"
+  session_affinity      = "NONE"
+  timeout_sec           = 30
+  backend {
+    group           = google_compute_region_instance_group_manager.compute_instance_group_manager.instance_group
+    balancing_mode  = "UTILIZATION"
+    capacity_scaler = 1.0
+  }
+}
